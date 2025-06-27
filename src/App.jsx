2fc5d7d6da127878f27ab0a3165012e25a68f178
MaxNossevitch <maxim.nossevitch@gmail.com>
2025-06-27 11:37:26 -0400

    visual updates, layout changes to projects section

--- src/App.jsx
+++ src/App.jsx
@@ -174,13 +174,18 @@ function App() {
                             key={`project-${index}`}
                             className="project-item p-4 sm:w-full text-left"
                         >
-                            <span>
-                                <a href={project.url}><h3 className="text-xl font-semibold text-blue-600 hover:text-blue-400 transition hover:cursor-pointer">
-                                    {project.title}
-                                </h3></a>
-                                <a className="repo-link" href={project.repo}>{t('projectsRepo')}</a>
-                            </span>
-                            <p className="mt-2">{project.desc}</p>
+                            <div className="project-img">
+                                <img src={project.img} alt={project.title} />
+                            </div>
+                            <div className="project-details lg:px-10 sm:px-7">
+                                <span>
+                                    <a href={project.url}><h3 className="text-xl font-semibold text-blue-600 hover:text-blue-400 transition hover:cursor-pointer">
+                                        {project.title}
+                                    </h3></a>
+                                    <a className="repo-link" href={project.repo}>{t('projectsRepo')}</a>
+                                </span>
+                                <p className="mt-2">{project.desc}</p>
+                            </div>
                         </div>
                     ))}
                     </div>
